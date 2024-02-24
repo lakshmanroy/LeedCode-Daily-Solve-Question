7ce2fd01ac11f9475b539fd3750ccfc094d5ac2b
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-02-24 14:50:03 +0530

    commit

---
+++ ProblemList/Two_Sum.java
@@ -0,0 +1,22 @@
+package ProblemList;
+
+import java.util.HashMap;
+
+class Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 2, 7, 11, 15 };
+        int target = 9;
+        System.out.println(target);
+    }
+}
