1b822437d3c0b2eee3fb7fd7c0f13ab906df592b
Lakshman kumar <lakshmankumar.it2021@dscet.ac.in>
2024-05-17 09:51:47 +0530

    commit

---
+++ ProblemList/tempCodeRunnerFile.java
@@ -0,0 +1,22 @@
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String strs[]) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int index = 1; index < strs.length; index++) {
+            while (strs[index].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String strs[] = { "dog", "racecar", "car" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
