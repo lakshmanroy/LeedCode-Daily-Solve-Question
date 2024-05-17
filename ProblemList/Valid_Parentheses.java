69a05eb710df8b670f6da8b253e859b84e85fd98
Lakshman kumar <lakshmankumar.it2021@dscet.ac.in>
2024-05-17 10:43:54 +0530

    commit

---
+++ ProblemList/Valid_Parentheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(isValid(s));
+    }
+}
--- ProblemList/tempCodeRunnerFile.java
+++ ProblemList/tempCodeRunnerFile.java
@@ -1,4 +1,4 @@
-public class LongestCommonPrefix {
+public class tempCodeRunnerFile {
     public static String longestCommonPrefix(String strs[]) {
         if (strs == null || strs.length == 0) {
             return "";
