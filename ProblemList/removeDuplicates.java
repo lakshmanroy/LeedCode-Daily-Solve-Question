fe64f29b183d115d301dd449b4ee202f95c921a6
Lakshman kumar <lakshmankumar.it2021@dscet.ac.in>
2024-05-20 10:06:11 +0530

    commit

---
+++ ProblemList/removeDuplicates.java
@@ -0,0 +1,18 @@
+public class removeDuplicates {
+    public static int removeDuplicat(int nums[]) {
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 1, 2 };
+
+        System.out.println(removeDuplicat(nums));
+    }
+}
