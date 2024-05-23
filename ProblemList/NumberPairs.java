3cc59b952428fa80491595760f669e535eb249b7
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-23 17:39:55 +0530

    commit

---
+++ ProblemList/NumberPairs.java
@@ -0,0 +1,20 @@
+public class NumberPairs {
+    public static int pairs(int nums[]) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 1, 1, 3 };
+
+        System.out.println(pairs(nums));
+
+    }
+}
