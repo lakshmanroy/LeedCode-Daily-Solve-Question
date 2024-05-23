f272443f04c926469a1ad5ca7e9d3198c9bb154b
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-23 18:02:44 +0530

    commit

---
+++ ProblemList/HappyNumber.java
@@ -0,0 +1,20 @@
+public class HappyNumber {
+    public static boolean isHappy(int n) {
+        while (n > 9) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 100;
+            }
+            n = sum;
+        }
+        return n == 1 || n == 7;
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+
+        System.out.println(isHappy(n));
+    }
+}
