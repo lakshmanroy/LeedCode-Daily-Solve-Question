dad2f061c676622e62d8fb8dc81b15a525feacbf
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-25 10:13:39 +0530

    commit

---
+++ ProblemList/UglyNumber.java
@@ -0,0 +1,23 @@
+public class UglyNumber {
+    public static boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+
+        System.out.println(isUgly(n));
+    }
+}
