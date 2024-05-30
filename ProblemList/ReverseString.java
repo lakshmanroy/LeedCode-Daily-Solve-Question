904d0f73a9d733bc0e66258ff55e30310bac2482
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-30 10:41:47 +0530

    commit

---
+++ ProblemList/ReverseString.java
@@ -0,0 +1,35 @@
+public class ReverseString {
+    public static void reverseString(char s[]) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void printArr(char arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        char s[] = { 'H', 'e', 'l', 'l', 'o' };
+
+        reverseString(s);
+        printArr(s);
+
+    }
+}
