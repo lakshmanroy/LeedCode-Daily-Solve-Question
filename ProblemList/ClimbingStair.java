29814c3e516a08716e0c5c53b8993547b5da46b1
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-12-26 12:13:07 +0530

    commit

---
+++ ProblemList/ClimbingStair.java
@@ -0,0 +1,20 @@
+public class ClimbingStair {
+    public static int staircase(int n){
+        if(n<=1){
+            return 1;
+        }
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=5;
+
+        System.out.println(staircase(n));
+    }
+}
